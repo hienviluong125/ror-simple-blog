081092dc5172ca35db5eb355d1b380c0137eb2b2
hienviluong125 <hienviluong125@gmail.com>
2020-11-21 11:03:50 +0700

    integrate rspec to implement testing flow

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+  describe 'get #index' do
+    def do_request
+      get :index
+    end
+
+    context 'access to index page' do
+      before { do_request }
+
+      it { expect(response).to render_template :index }
+    end
+  end
+end
